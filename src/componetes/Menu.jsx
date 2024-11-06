a25941f52eabef20bcfa09b3ea47fedb5c9f5a43
Hariyad1 <hariyadi3663@gmail.com>
2024-11-06 23:03:32 +0700

    merubah link itemu

--- src/componetes/Menu.jsx
+++ src/componetes/Menu.jsx
@@ -23,13 +23,13 @@ const handleLogout=async()=>{
   }
 }
   return (
-    <div className="bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
+    <div className="bg-black w-[150px] z-10 flex flex-col items-start absolute top-12 right-2 md:right-8 rounded-md p-4 space-y-4">
     {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/login">Login</Link></h3>}
     {!user &&<h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/register">Register</Link></h3>}
     {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/profile/"+user._id}>My Profile</Link></h3>}
     {user &&<h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/write">Write</Link></h3>}
     {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myposts/"+user._id}>My Posts</Link></h3>}
-    {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/item"}>Items</Link></h3>}
+    {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/Item"}>Items</Link></h3>}
     {user &&<h3 onClick={handleLogout} className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
 
     </div>
