121bba57b2094c86006bf46aa1463e8ad11bf824
Hariyad1 <hariyadi3663@gmail.com>
2024-11-16 21:28:56 +0700

    Menambahkan halaman tentang

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+const TentangKamiPage = () => {
+    return (
+      <div className="mt-8 w-full bg-gray-100 px-8 py-8 text-gray-800">
+        <h1 className="text-3xl font-bold mb-4">Tentang Kami</h1>
+        <p className="text-md mb-4">
+          Selamat datang di Jejak Hilang, platform yang didedikasikan untuk membantu Anda dalam pelaporan dan pencarian barang hilang. 
+          Kami berkomitmen untuk menyediakan layanan yang cepat dan efektif untuk memudahkan proses pencarian barang Anda.
+        </p>
+        <p className="text-md mb-4">
+          Dengan teknologi terkini dan jaringan yang luas, kami berusaha untuk menghubungkan orang-orang yang kehilangan barang dengan mereka yang menemukannya. 
+          Kami percaya bahwa setiap barang berharga dan layak untuk ditemukan kembali.
+        </p>
+        <p className="text-md">
+          Hubungi kami untuk informasi lebih lanjut tentang bagaimana kami dapat membantu Anda menemukan barang yang hilang atau melaporkan barang yang ditemukan.
+        </p>
+      </div>
+    );
+  };
+  
+  export default TentangKamiPage;
