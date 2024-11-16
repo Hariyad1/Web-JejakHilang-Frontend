4126dfb245dcefe470f2182123d940a5912284e5
Hariyad1 <hariyadi3663@gmail.com>
2024-11-16 21:03:39 +0700

    Menambahkan ProtectedRoute untuk Auntentifikasi

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,33 @@ import CreatePost from "./pages/CreatePost"
 import EditPost from "./pages/EditPost"
 import Profile from "./pages/Profile"
 import MyPosts from "./pages/MyPosts"
+import AdminDashboard from "./pages/AdminDashboard"
+import About from "./pages/About"
+import Service from "./pages/Service"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
+import TermsAndConditions from "./pages/TermsandConditions"
 import { UserContextProvider } from "./context/UserContext"
-
+import ProtectedRoute from "./context/ProtectedRoute"
 
 const App = () => {
   return (
     <UserContextProvider>
         <Routes>
         <Route exact path ="/item" element={<Item/>}/>
-        <Route exact path ="/others" element={<Others/>}/>
+        <Route exact path ="/others" element={<ProtectedRoute allowedRoles={['user']}><Others/></ProtectedRoute>}/>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
-        <Route exact path="/write" element={<CreatePost/>}/>
-        <Route exact path="/edit/:id" element={<EditPost/>}/>
-        <Route exact path="/myposts/:id" element={<MyPosts/>}/>
-        <Route exact path="/posts/post/:id" element={<PostDetailsItem/>}/>
-        <Route exact path="/profile/:id" element={<Profile/>}/>
+        <Route exact path="/write" element={<ProtectedRoute allowedRoles={['user']}><CreatePost/></ProtectedRoute>}/>
+        <Route exact path="/edit/:id" element={<ProtectedRoute allowedRoles={['user']}><EditPost/></ProtectedRoute>}/>
+        <Route exact path="/myposts/:id" element={<ProtectedRoute allowedRoles={['user']}><MyPosts/></ProtectedRoute>}/>
+        <Route exact path="/posts/post/:id" element={<ProtectedRoute allowedRoles={['user']}><PostDetailsItem/></ProtectedRoute>}/>
+        <Route exact path="/profile/:id" element={<ProtectedRoute allowedRoles={['user']}><Profile/></ProtectedRoute>}/>
+        <Route exact path="/admin" element={<ProtectedRoute allowedRoles={['admin']}><AdminDashboard/></ProtectedRoute>}/>
+        <Route exact path="/about" element={<About/>}/>
+        <Route exact path="/service" element={<Service/>}/>
+        <Route exact path="/privacy-policy" element={<PrivacyPolicy/>}/>
+        <Route exact path="/terms-and-conditions" element={<TermsAndConditions/>}/>
         </Routes>
     </UserContextProvider>
   )
