641462ad8e2e143b4ce1a77f92cf7cfd0c4d904a
Hariyad1 <hariyadi3663@gmail.com>
2024-11-21 21:50:30 +0700

    Perubahan dan penambahan style sectioncategorize

--- src/component/SectionCategorize.jsx
+++ src/component/SectionCategorize.jsx
@@ -38,13 +38,13 @@ const SectionCatagorize = () => {
       <div className="flex inset-0 bg-black opacity-50"></div>
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center text-black">
-          <h2 className="text-3xl font-extrabold mb-10 mt-4">Temukan Barang Hilang Anda dari Sini</h2>
+          <h2 className="text-3xl font-extrabold mb-10 mt-4">Temukan atau Laporkan Barang Hilang dari Sini</h2>
           <p className="text-lg mt-4 mb-10">Buat Postingan dan Cari Barang Hilang Anda di Sini</p>
           <Link
             to="/item"
             className="mt-10 px-6 py-2 bg-blue-600 rounded-full hover:bg-blue-700 text-white text-sm font-semibold transition duration-300 ease-in-out"
           >
-            Explore Now
+            Eksplor Sekarang
           </Link>
         </div>
       </div>
