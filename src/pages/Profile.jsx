7e9e020f9fbf1ad3d0c3c6c82d96f923e4407af1
Hariyad1 <hariyadi3663@gmail.com>
2024-11-21 21:59:41 +0700

    Perubahan dan penambahan alert pada auth

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,9 @@ import ProfilePosts from "../component/ProfilePosts";
 import axios from "axios";
 import { IF, URL } from "../url";
 import { UserContext } from "../context/UserContext";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { UserOutlined, MailOutlined, EyeOutlined } from "@ant-design/icons";
+import { Table } from "antd";
 
 const Profile = () => {
   const param = useParams().id;
@@ -16,6 +18,17 @@ const Profile = () => {
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   const [updated, setUpdated] = useState(false);
+
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 5,
+  });
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  const handleTableChange = (pagination) => {
+    setPagination(pagination);
+  };
 
   const fetchProfile = async () => {
     if (!user || !user._id) return;
@@ -82,33 +95,106 @@ const Profile = () => {
     fetchUserPosts();
   }, [param, user]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const columns = [
+    {
+      title: 'No',
+      key: 'index',
+      render: (text, record, index) => (pagination.current - 1) * pagination.pageSize + index + 1,
+    },
+    {
+      title: 'Image',
+      dataIndex: 'photo',
+      key: 'photo',
+      render: (text, record) => (
+        record.photo ? <img src={record.photo} alt={record.title} style={{ width: 50, height: 50 }} /> : 'No Image'
+      ),
+    },
+    {
+      title: 'Title',
+      dataIndex: 'title',
+      key: 'title',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'desc',
+      key: 'desc',
+      className: 'hidden md:table-cell',
+      render: (text) => (
+        <span>
+          {text.length > 250 ? `${text.substring(0, 250)}...` : text}
+        </span>
+      ),
+    },
+    {
+      title: 'Actions',
+      key: 'actions',
+      render: (text, record) => (
+        <span className="flex items-center">
+          <Link
+            to={user ? `/posts/post/${record._id}` : "/login"}
+            className="flex items-center px-2 py-1 border border-gray-300 rounded-lg hover:bg-gray-100 transition"
+          >
+            <EyeOutlined className="mr-2" />
+            View
+          </Link>
+        </span>
+      ),
+    },
+  ];
+
   return (
     <div className="bg-gray-100">
       <Navbar />
-      <div className="min-h-screen px-4 md:px-8 md:space-x-8 mt-8 flex flex-col md:flex-row z-10">
+      <div className="min-h-screen px-4 md:px-8 md:space-x-8 mt-8 flex mx-0 md:mx-2 lg:mx-32 flex-col md:flex-row z-10">
         <div className="md:w-[70%] w-full md:mt-0 mb-8 md:mb-0">
           <h1 className="text-2xl md:text-3xl font-bold mb-4">Your Posts:</h1>
-          {posts?.map((p) => (
-            <ProfilePosts key={p._id} p={p} />
-          ))}
+          <Table
+            dataSource={posts}
+            columns={columns}
+            rowKey="_id"
+            pagination={{
+              ...pagination,
+              showTotal: (total, range) => `Total items: ${total}`,
+              showSizeChanger: true,
+              pageSizeOptions: ['5', '10', '20'],
+            }}
+            onChange={handleTableChange}
+          />
         </div>
         <div className="md:sticky md:top-12 md:w-[30%] w-full md:items-end">
           <div className="flex flex-col space-y-4 items-start p-4 bg-white rounded-lg shadow-md">
             <h1 className="text-2xl font-bold mb-4">Profile</h1>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              value={username}
-              className="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-              placeholder="Your Username"
-              type="text"
-            />
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              className="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
-              placeholder="Your Email"
-              type="email"
-            />
+            <div className="flex items-center space-x-2">
+              <UserOutlined />
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                className="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                placeholder="Your Username"
+                type="text"
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <MailOutlined />
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                className="px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+                placeholder="Your Email"
+                type="email"
+              />
+            </div>
             <div className="flex items-center space-x-4 mt-8">
               <button
                 onClick={handleUserUpdate}
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,68 @@
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "../component/Footer";
-import { useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import axios from "axios";
 import { URL } from "../url";
+import { UserOutlined, MailOutlined, LockOutlined } from '@ant-design/icons';
+import Swal from 'sweetalert2';
+import { UserContext } from "../context/UserContext";
+import '../App.css';
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
+  const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+
   const handleRegister = async () => {
-    // Validasi format email
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!emailPattern.test(email)) {
-      alert("Please enter a valid email address.");
+      Swal.fire({
+        icon: 'warning',
+        title: 'Oops...',
+        text: 'Please enter a valid email address.',
+        showConfirmButton: true,
+        customClass: {
+          confirmButton: 'swal-button'
+        }
+      });
       return;
     }
 
     try {
       const res = await axios.post(URL + "/api/auth/register", { username, email, password });
-      setUsername(res.data.username);
-      setEmail(res.data.email);
-      setPassword(res.data.password);
+      Swal.fire({
+        icon: 'success',
+        title: 'Registration Successful',
+        text: 'You can now log in with your new account.',
+        showConfirmButton: true,
+        customClass: {
+          confirmButton: 'swal-button'
+        }
+      });
+      setUsername("");
+      setEmail("");
+      setPassword("");
       setError(false);
       navigate("/login");
     } catch (err) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Registration Failed',
+        text: 'Something went wrong. Please try again.',
+        showConfirmButton: true,
+        customClass: {
+          confirmButton: 'swal-button'
+        }
+      });
       setError(true);
       console.log(err);
     }
@@ -34,33 +70,51 @@ const Register = () => {
 
   return (
     <div className="flex flex-col h-screen bg-gray-100">
-      <header className="p-4  flex justify-between items-center bg-gradient-to-r from-blue-500 to-purple-500">
-        <h1 className="text-lg md:text-3xl pl-20 font-extrabold">
-          <Link to="/">Temukan Barang Hilang Anda</Link>
+      <header className="p-4 flex justify-between items-center bg-gradient-to-r from-blue-500 to-purple-500 mb-4">
+      <h1 className="text-lg md:text-3xl font-extrabold mr-4 pl-0 pr-0 md:pl-20 md:pr-0">
+          <Link to="/"> Temukan atau Laporkan Barang Hilang Anda</Link>
         </h1>
-        <Link to="/login" className="text-white pr-20 hover:underline">Masuk</Link>
+        <Link to="/login" className="text-white hover:underline ml-4 pl-0 pr-0 md:pr-20">Masuk</Link>
       </header>
       <main className="flex-1 flex justify-center items-center">
         <div className="w-full md:w-1/3 p-4 bg-white shadow-md rounded-lg">
           <h1 className="text-2xl font-bold mb-4 text-center">Daftarkan Akun Anda</h1>
-          <input
-            onChange={(e) => setUsername(e.target.value)}
-            className="w-full px-4 py-2 mb-3 border rounded-lg focus:outline-none"
-            type="text"
-            placeholder="Masukkan Username"
-          />
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-4 py-2 mb-3 border rounded-lg focus:outline-none"
-            type="text"
-            placeholder="Masukkan Email"
-          />
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 mb-3 border rounded-lg focus:outline-none"
-            type="password"
-            placeholder="Masukkan Password"
-          />
+          <label className="block text-sm font-medium text-gray-700 mb-2">Username</label>
+          <div className="relative mb-3">
+            <span className="absolute top-2/4 transform -translate-y-2/4 left-0 flex items-center pl-3 pointer-events-none">
+              <UserOutlined className="text-gray-400" />
+            </span>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full pl-10 py-2 border rounded-lg focus:outline-none"
+              type="text"
+              placeholder="Masukkan Username"
+            />
+          </div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">Email</label>
+          <div className="relative mb-3">
+            <span className="absolute top-2/4 transform -translate-y-2/4 left-0 flex items-center pl-3 pointer-events-none">
+              <MailOutlined className="text-gray-400" />
+            </span>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full pl-10 py-2 border rounded-lg focus:outline-none"
+              type="text"
+              placeholder="Masukkan Email"
+            />
+          </div>
+          <label className="block text-sm font-medium text-gray-700 mb-2">Password</label>
+          <div className="relative mb-3">
+            <span className="absolute top-2/4 transform -translate-y-2/4 left-0 flex items-center pl-3 pointer-events-none">
+              <LockOutlined className="text-gray-400" />
+            </span>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full pl-10 py-2 border rounded-lg focus:outline-none"
+              type="password"
+              placeholder="Masukkan Password"
+            />
+          </div>
           <button
             onClick={handleRegister}
             className="w-full px-4 py-2 text-lg font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600"
