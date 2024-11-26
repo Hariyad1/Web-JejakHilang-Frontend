5132765e1ccb16b80e6a20161ec0cea6985e4e50
Hariyad1 <hariyadi3663@gmail.com>
2024-11-26 22:51:38 +0700

    Menambahkan halaman notfound

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Maaf, halaman yang Anda cari tidak ditemukan.</p>
+    </div>
+  );
+};
+
+export default NotFound;
