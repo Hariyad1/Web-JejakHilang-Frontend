e06898604f0ce10b68fdede73e4533e809c90e9f
Hariyad1 <hariyadi3663@gmail.com>
2024-12-03 18:27:23 +0700

    Memperbarui url

--- src/url.js
+++ src/url.js
@@ -1,2 +1,2 @@
-export const URL = "http://web-jejakhilang-backend.up.railway.app"
+export const URL = "https://web-jejakhilang-backend.up.railway.app"
 export const IF = "https://ik.imagekit.io/jejakhilang";
